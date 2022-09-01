08a6e88593118fae4de2a6315ba5657e8d1f554d
Izabela Blinkiewicz <iza.marciniak123@gmail.com>
2022-09-01 19:15:29 +0200

    If statments added

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-alert("Hello World:");
+var youLikeMeat = false;
+var myNum = 8;
+
+if (youLikeMeat){
+
+    document.write("you like meat");
+
+} else{
+
+    document.write("you dont like meat");
+}
